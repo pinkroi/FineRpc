f6f990a8038037092b57c19d3cdccdec883855d7
shenfu.shen <shenfu.shen@alibaba-inc.com>
2024-07-03 16:58:49 +0800

    chore(pom.xml): 删除Lombok依赖项

--- src/main/java/com/sf/fine/rpc/provider/RpcProviderHandler.java
+++ src/main/java/com/sf/fine/rpc/provider/RpcProviderHandler.java
@@ -1,13 +1,11 @@
 package com.sf.fine.rpc.provider;
 
 import com.sf.fine.rpc.common.ServiceUtils;
-import com.sf.fine.rpc.consumer.RpcConsumerBean;
 import com.sf.fine.rpc.protocol.RpcRequest;
 import com.sf.fine.rpc.protocol.RpcResponse;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
-import lombok.extern.slf4j.Slf4j;
 import net.sf.cglib.reflect.FastClass;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
